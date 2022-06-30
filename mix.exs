36421d74bcb45af30cd03999ef8a834054ba92b3
vanppo <vanppo@hotmail.com>
2022-06-30 14:03:35 +0800

    chore: integrate credo

--- mix.exs
+++ mix.exs
@@ -6,13 +6,22 @@ defmodule Plumber.MixProject do
       app: :plumber,
       version: "0.1.0",
       elixir: "~> 1.13",
-      test_coverage: [summary: [threshold: 80]]
+      deps: deps(),
+      test_coverage: [
+        summary: [threshold: 80]
+      ]
     ]
   end
 
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp deps() do
+    [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 end
